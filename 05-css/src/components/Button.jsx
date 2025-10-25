dc45ffb84d3f691c63aa4b7e607473fff0f031c9
Rajan048 <rajansaini2.0.0.6@gmail.com>
2025-10-25 18:44:29 +0530

    05-css Added

---
+++ 05-css/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+const Button = () => {
+  return (
+    <div className='parent'>
+      <div className="child">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, ab!</p>
+        <button className='btn'>click me!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        logo
+      </div>
+
+      <div className="feature">
+        <button className='btn'>Home</button>
+        <button className='btn'>About</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
